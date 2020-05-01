50f45981b9c959aa9b9c14089b45f64edf0fa7dd
Shridhar Ulli <shridharulli@gmail.com>
2020-05-01 08:14:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ class View {
 
     this.title = this.createElement("h1");
     this.title.textContent = "📝 Todos";
-    this.title.classList.add("text-4xl", "text-blue-500", "font-bold", "mb-5");
+    this.title.classList.add("text-4xl", "text-blue-700", "font-bold", "mb-5");
 
     this.form = this.createElement("form");
 
@@ -64,7 +64,7 @@ class View {
     this.submitButton = this.createElement("button");
     this.submitButton.textContent = "Submit";
     this.submitButton.classList.add(
-      "bg-blue-600",
+      "bg-blue-700",
       "px-5",
       "py-3",
       "text-gray-100"
@@ -140,10 +140,10 @@ class View {
         const deleteButton = this.createElement("button", "delete");
         deleteButton.textContent = "X";
         deleteButton.classList.add(
-          "bg-red-500",
+          "bg-red-600",
           "appearance-none",
           "border-2",
-          "border-red-400",
+          "border-red-600",
           "py-2",
           "px-4",
           "text-gray-100",
